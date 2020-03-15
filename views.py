9131b0d163bfe22e953c10443049a4cadc0f5383
Sumit Kant Verma <imskv420@gmail.com>
2020-03-15 20:38:32 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,96 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profile_api import serializers
+from rest_framework import viewsets
+from profile_api import models
+from rest_framework.authentication import TokenAuthentication
+from profile_api import permissions
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
+
+class HelloApiView(APIView):
+    serializer_class=serializers.HelloSerializer
+    def get(self,request,format=None):
+        an_apiview=[
+            'sumit',
+            'Amit'
+        ]
+        return Response({'message':'hello','an_apiview':an_apiview})
+
+    def post(self,request):
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_404_BAD_REQUEST
+            )
+    def put(self,request,pk=None):
+        return Response({'method':'PUT'})
+    def patch(self,request,pk=None):
+        return Response({'method':'PATCH'})
+    def delete(self,request,pk=None):
+        return Response({'method':'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class=serializers.HelloSerializer
+
+    def list(self,request):
+        a_viewset=[
+            'Srishti',
+            'Khushi'
+        ]
+        return Response({'message':'Hello','a_viewset':a_viewset})
+
+    def create(self,request):
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_404_BAD_REQUEST
+            )
+    def retrieve(self,request,pk=None):
+        return Response({'http_method':'GET'})
+    def update(self,request,pk=None):
+        return Response({'http_method':'PUT'})
+    def partial_update(self,request,pk=None):
+        return Response({'http_method':'PATCH'})
+    def destroy(self,request,pk=None):
+        return Response({'http_method':'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(permissions.UpdateOwnProfile,)
+    filter_backends=(filters.SearchFilter,)
+    search_fields=('name','email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes=api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    authentication_classes=(TokenAuthentication,)
+    serializer_class=serializers.ProfileFeedItemSerializer
+    queryset=models.ProfileFeedItem.objects.all()
+    permission_classes=(
+        permissions.UpdateOwnStatus,
+        IsAuthenticatedOrReadOnly,
+        IsAuthenticated
+    )
+
+    def perform_create(self,serializer):
+        serializer.save(user_profile=self.request.user)
